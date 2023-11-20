3d6486b296ed6593252ff17f16dbc968f28463da
Vishal <vishalpal1501@gmail.com>
2023-11-20 23:16:41 +0530

    create Router usinf createBrowserRouter and RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
-  return <div className="App"></div>;
+  const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
+    { path: '/contact', element: <Contact /> },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
