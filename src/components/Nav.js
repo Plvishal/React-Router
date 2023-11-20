d821dadbb362e0f564251931e4fa1e892ab1076d
Vishal <vishalpal1501@gmail.com>
2023-11-21 00:06:40 +0530

    css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 function Nav() {
   return (
     <>
-      <div className="flex bg-rose-400  justify-evenly p-4 m-2 rounded-lg shadow-md">
+      <div className="flex bg-rose-400  justify-evenly p-4 m-2 rounded-lg shadow-md flex-wrap flex-1">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
