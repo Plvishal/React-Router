7977d92c524e5776373eee628f8bb508318fe9fa
Vishal <vishalpal1501@gmail.com>
2023-11-21 14:27:14 +0530

    useNavigate inside the error page back to home page after some second

--- src/pages/ErrorHandling.js
+++ src/pages/ErrorHandling.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorHandling() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => navigate('/'), 4000);
+  });
   return (
     <div>
       <h1>Something wring!!!!!!</h1>
